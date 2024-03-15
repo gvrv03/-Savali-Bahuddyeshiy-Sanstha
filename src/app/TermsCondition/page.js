707c9ac030c9cda49389c4c490c5bedea619863d
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2024-03-15 19:07:53 +0530

    Add HTML Entities

--- src/app/TermsCondition/page.js
+++ src/app/TermsCondition/page.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TermsCondition = () => {
     return (
-        <div  className='mt-[92px] p-5 md:p-0  container m-auto text-justify'>
+        <div className='mt-[92px] p-5 md:p-0 container m-auto text-justify'>
             <h2><strong>Terms and Conditions</strong></h2>
 
             <p>Welcome to Savali Bahuddyeshiy Sanstha!</p>
@@ -11,7 +11,7 @@ const TermsCondition = () => {
 
             <p>By accessing this website we assume you accept these terms and conditions. Do not continue to use Savali Bahuddyeshiy Sanstha if you do not agree to take all of the terms and conditions stated on this page.</p>
 
-            <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company's terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client's needs in respect of provision of the Company's stated services, in accordance with and subject to, prevailing law of in. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
+            <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: &quot;Client&quot;, &quot;You&quot; and &quot;Your&quot; refers to you, the person log on this website and compliant to the Company's terms and conditions. &quot;The Company&quot;, &quot;Ourselves&quot;, &quot;We&quot;, &quot;Our&quot; and &quot;Us&quot;, refers to our Company. &quot;Party&quot;, &quot;Parties&quot;, or &quot;Us&quot;, refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client's needs in respect of provision of the Company's stated services, in accordance with and subject to, prevailing law of in. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
 
             <h3><strong>Cookies</strong></h3>
 
