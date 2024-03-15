2dac3190d5e00472bc00cb2ec3c9b166a0506046
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2024-03-15 19:11:59 +0530

    Add HTML Entities 2

--- src/app/TermsCondition/page.js
+++ src/app/TermsCondition/page.js
@@ -7,17 +7,17 @@ const TermsCondition = () => {
 
             <p>Welcome to Savali Bahuddyeshiy Sanstha!</p>
 
-            <p>These terms and conditions outline the rules and regulations for the use of Savali Bahuddyeshiy Sanstha's Website, located at savalibahuddyeshiysanstha.com.</p>
+            <p>These terms and conditions outline the rules and regulations for the use of Savali Bahuddyeshiy Sanstha Website, located at savalibahuddyeshiysanstha.com.</p>
 
             <p>By accessing this website we assume you accept these terms and conditions. Do not continue to use Savali Bahuddyeshiy Sanstha if you do not agree to take all of the terms and conditions stated on this page.</p>
 
-            <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: &quot;Client&quot;, &quot;You&quot; and &quot;Your&quot; refers to you, the person log on this website and compliant to the Company's terms and conditions. &quot;The Company&quot;, &quot;Ourselves&quot;, &quot;We&quot;, &quot;Our&quot; and &quot;Us&quot;, refers to our Company. &quot;Party&quot;, &quot;Parties&quot;, or &quot;Us&quot;, refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client's needs in respect of provision of the Company's stated services, in accordance with and subject to, prevailing law of in. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
+            <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: &quot;Client&quot;, &quot;You&quot; and &quot;Your&quot; refers to you, the person log on this website and compliant to the Company terms and conditions. &quot;The Company&quot;, &quot;Ourselves&quot;, &quot;We&quot;, &quot;Our&quot; and &quot;Us&quot;, refers to our Company. &quot;Party&quot;, &quot;Parties&quot;, or &quot;Us&quot;, refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client needs in respect of provision of the Company stated services, in accordance with and subject to, prevailing law of in. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
 
             <h3><strong>Cookies</strong></h3>
 
-            <p>We employ the use of cookies. By accessing Savali Bahuddyeshiy Sanstha, you agreed to use cookies in agreement with the Savali Bahuddyeshiy Sanstha's Privacy Policy. </p>
+            <p>We employ the use of cookies. By accessing Savali Bahuddyeshiy Sanstha, you agreed to use cookies in agreement with the Savali Bahuddyeshiy Sanstha Privacy Policy. </p>
 
-            <p>Most interactive websites use cookies to let us retrieve the user's details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.</p>
+            <p>Most interactive websites use cookies to let us retrieve the users details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.</p>
 
             <h3><strong>License</strong></h3>
 
@@ -60,7 +60,7 @@ const TermsCondition = () => {
                 <li>System wide Accredited Businesses except soliciting non-profit organizations, charity shopping malls, and charity fundraising groups which may not hyperlink to our Web site.</li>
             </ul>
 
-            <p>These organizations may link to our home page, to publications or to other Website information so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products and/or services; and (c) fits within the context of the linking party's site.</p>
+            <p>These organizations may link to our home page, to publications or to other Website information so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products and/or services; and (c) fits within the context of the linking party site.</p>
 
             <p>We may consider and approve other link requests from the following types of organizations:</p>
 
@@ -76,7 +76,7 @@ const TermsCondition = () => {
 
             <p>We will approve link requests from these organizations if we decide that: (a) the link would not make us look unfavorably to ourselves or to our accredited businesses; (b) the organization does not have any negative records with us; (c) the benefit to us from the visibility of the hyperlink compensates the absence of Savali Bahuddyeshiy Sanstha; and (d) the link is in the context of general resource information.</p>
 
-            <p>These organizations may link to our home page so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products or services; and (c) fits within the context of the linking party's site.</p>
+            <p>These organizations may link to our home page so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products or services; and (c) fits within the context of the linking party site.</p>
 
             <p>If you are one of the organizations listed in paragraph 2 above and are interested in linking to our website, you must inform us by sending an e-mail to Savali Bahuddyeshiy Sanstha. Please include your name, your organization name, contact information as well as the URL of your site, a list of any URLs from which you intend to link to our Website, and a list of the URLs on our site to which you would like to link. Wait 2-3 weeks for a response.</p>
 
@@ -85,10 +85,10 @@ const TermsCondition = () => {
             <ul>
                 <li>By use of our corporate name; or</li>
                 <li>By use of the uniform resource locator being linked to; or</li>
-                <li>By use of any other description of our Website being linked to that makes sense within the context and format of content on the linking party's site.</li>
+                <li>By use of any other description of our Website being linked to that makes sense within the context and format of content on the linking party site.</li>
             </ul>
 
-            <p>No use of Savali Bahuddyeshiy Sanstha's logo or other artwork will be allowed for linking absent a trademark license agreement.</p>
+            <p>No use of Savali Bahuddyeshiy Sanstha logo or other artwork will be allowed for linking absent a trademark license agreement.</p>
 
             <h3><strong>iFrames</strong></h3>
 
@@ -100,7 +100,7 @@ const TermsCondition = () => {
 
             <h3><strong>Reservation of Rights</strong></h3>
 
-            <p>We reserve the right to request that you remove all links or any particular link to our Website. You approve to immediately remove all links to our Website upon request. We also reserve the right to amen these terms and conditions and it's linking policy at any time. By continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions.</p>
+            <p>We reserve the right to request that you remove all links or any particular link to our Website. You approve to immediately remove all links to our Website upon request. We also reserve the right to amen these terms and conditions and it is linking policy at any time. By continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions.</p>
 
             <h3><strong>Removal of links from our website</strong></h3>
 
