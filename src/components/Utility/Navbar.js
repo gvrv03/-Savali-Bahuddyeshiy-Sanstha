7d8877f0a5da40f8492b2169c802c2feda5509e1
gvrv03 <itsgaurav3112003@gmail.com>
2025-03-06 16:49:29 +0530

    new Version Of Website 3

--- src/components/Utility/Navbar.js
+++ src/components/Utility/Navbar.js
@@ -45,7 +45,7 @@ export default function Navbar() {
       </div>
 
       {/* Navbar */}
-      <header className="bg-white shadow-sm sticky top-0 ">
+      <header className="bg-white shadow-sm sticky top-0 z-40">
         <div className="container mx-auto flex justify-between items-center py-4 px-4 md:px-8">
           <Link href="/" className="flex items-center">
             <Image
